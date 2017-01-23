43e54c256d5c277dca2a4c08356d8e06c717784a
Steve Kane <kanesteven@gmail.com>
2017-01-23 17:31:49 -0600

    1d spring equation now rendering on GPU ( and cpu example with logging )

--- cpurk4.js
+++ cpurk4.js
@@ -1,26 +1,62 @@
-function rk4 ( t, x, h, f ) {
-  const k1 = f(t, x)
-  const k2 = f(t + h / 2, x + h / 2 * k1)
-  const k3 = f(t + h / 2, x + h / 2 * k2)
-  const k4 = f(t + h, x + h * k3)
-  const x1 = x + ( h / 6 ) * ( k1 + 2 * k2 + 2 * k3 + k4 )
+function rk4 ( h, t, x, v, dx, dv ) {
+  const dx1 = dx(t, x, v)
+  const dv1 = dv(t, x, v)
+  const x1 = x + h / 2 * dx1
+  const v1 = v + h / 2 * dv1
+  const thOver2 = t + h / 2
 
-  return x1
+  const dx2 = dx(thOver2, x1, v1)
+  const dv2 = dv(thOver2, x1, v1)
+  const x2 = x + h / 2 * dx2 
+  const v2 = v + h / 2 * dv2 
+
+  const dx3 = dx(thOver2, x2, v2)
+  const dv3 = dv(thOver2, x2, v2)
+  const x3 = x + h * dx3
+  const v3 = v + h * dv3
+
+  const dx4 = dx(t + h, x3, v3)
+  const dv4 = dv(t + h, x3, v3)
+
+  const xOut = x + ( h / 6 ) * ( dx1 + 2 * dx2 + 2 * dx3 + dx4 )
+  const vOut = v + ( h / 6 ) * ( dv1 + 2 * dv2 + 2 * dv3 + dv4 )
+
+  return [ xOut, vOut ]
 }
 
-function yPrime ( t, y ) {
-  return t * Math.sqrt(y)
+function dv ( k, m, d, t, x, v ) {
+  return -( k / m ) * x - ( d / m ) * v
 }
 
-function actual ( t ) {
-  return ( 1 / 16 ) * ( t * t + 4 ) * ( t * t + 4 );
+function dx ( t, x, v ) {
+  return v
 }
 
+var K = 3
+var MASS = 0.5
+var DAMPING = 0.1
 var t = 0 
-var y = 1
-var h = 0.05
+var x = 1
+var v = 0.2
+var h = 0.1
+var dv1 = dv.bind(null, K, MASS, DAMPING)
 
-for ( ; t <= 1; t += h ) {
-  console.log(`~${ y }\t=${ actual(t) } at ${ t }`)
-  y = rk4(t, y, h, yPrime)
+for ( var i = 0; i <= 100; i++, t += h ) {
+  renderToConsole(.1, t, x)
+  var out = rk4(h, t, x, v, dx, dv1)
+
+  x = out[0]
+  v = out[1]
+}
+
+function renderToConsole ( xScale, t, x ) {
+  var xBarMagnitude = x / xScale | 0
+  var xBar = ''
+  var tVal = t.toPrecision(1)
+  var xVal = x.toPrecision(4)
+
+  for ( var i = 0; i < xBarMagnitude; i++ ) {
+    xBar += '*' 
+  }
+  console.log(`t: ${tVal}, \tx: ${xVal} \t${xBar}`)
 }
--- index.js
+++ index.js
@@ -4,44 +4,6 @@ var FULL_SCREEN_QUAD = [
   -1, 1, 1, -1, 1, 1 
 ]
 
-var glsl_rk4 = `
-  #define F(t, x) t * sqrt(x)
-
-  float rk4 ( float t, float x, float h ) {
-    float k1 = F(t, x);
-    float k2 = F(t + h / 2., x + h / 2. * k1);
-    float k3 = F(t + h / 2., x + h / 2. * k2);
-    float k4 = F(t + h, x + h * k3);
-    float x1 = x + ( h / 6. ) * ( k1 + 2. * k2 + 2. * k3 + k4 );
-
-    return x1;
-  }
-`
-
-var SIDE = 2
-var p0 = new Float32Array(SIDE * SIDE * 4)
-
-for ( var i = 0; i < p0.length; i+=4 ) {
-  p0[i] = i
-}
-
-var t = 0
-var h = 0.1
-var src = regl.framebuffer({
-  width: SIDE,
-  height: SIDE,
-  data: p0,
-  colorType: 'float',
-  depth: false
-})
-var dest = regl.framebuffer({
-  width: SIDE,
-  height: SIDE,
-  colorType: 'float',
-  depth: false
-})
-var tmp = null
-
 var step = regl({
   vert: `
     attribute vec2 pos;
@@ -53,44 +15,146 @@ var step = regl({
   frag: `
     precision mediump float;
 
-    ${glsl_rk4}
-
     uniform float t;
     uniform float h;
+    uniform float k;
+    uniform float mass;
+    uniform float damping;
+    uniform float side;
     uniform sampler2D src;
 
-    const vec2 shape = vec2(${SIDE}.);
+    float dv ( float t, float x, float v ) {
+      return -( k / mass ) * x - ( damping / mass ) * v;
+    }
+
+    float dx ( float t, float x, float v ) {
+      return v;
+    }
 
     void main () {
-      vec2 index = gl_FragCoord.xy / shape;
-      vec4 i = texture2D(src, index);
-      float xt1 = rk4(t, i.x, h);
+      vec2 index = gl_FragCoord.xy / side;
+      vec4 previous = texture2D(src, index);
 
-      gl_FragColor = vec4(xt1, 0, 0, 0); 
+      float x = previous.x;
+      float v = previous.y;
+
+      float dx1 = dx(t, x, v);
+      float dv1 = dv(t, x, v);
+      float x1 = x + h / 2. * dx1;
+      float v1 = v + h / 2. * dv1;
+      float thOver2 = t + h / 2.;
+
+      float dx2 = dx(thOver2, x1, v1);
+      float dv2 = dv(thOver2, x1, v1);
+      float x2 = x + h / 2. * dx2;
+      float v2 = v + h / 2. * dv2;
+
+      float dx3 = dx(thOver2, x2, v2);
+      float dv3 = dv(thOver2, x2, v2);
+      float x3 = x + h * dx3;
+      float v3 = v + h * dv3;
+
+      float dx4 = dx(t + h, x3, v3);
+      float dv4 = dv(t + h, x3, v3);
+      float xOut = x + ( h / 6. ) * ( dx1 + 2. * dx2 + 2. * dx3 + dx4 );
+      float vOut = v + ( h / 6. ) * ( dv1 + 2. * dv2 + 2. * dv3 + dv4 );
+
+      gl_FragColor = vec4(xOut, vOut, 0, 0);
     } 
   `,
   attributes: {
     pos: FULL_SCREEN_QUAD
   },
   uniforms: {
-    src: regl.prop('src'),
-    h: regl.prop('h'),
-    t: regl.prop('t')
+    h:       regl.prop('h'),
+    t:       regl.prop('t'),
+    k:       regl.prop('k'),
+    mass:    regl.prop('mass'),
+    damping: regl.prop('damping'),
+    side:    regl.prop('side'),
+    src:     regl.prop('src')
   },
   count: 6,
   framebuffer: regl.prop('dest') 
 })
 
-// for ( ; t <= 1; t += h ) {
-//   tmp = src
-//   src = dest
-//   dest = tmp
-//   step({ src, dest, t, h })
-// }
+var draw = regl({
+  vert: `
+    uniform sampler2D src;
 
-step({ src, dest, t, h })
-step({ dest, src, t: t + h, h })
-regl({ framebuffer: src })(_ => {
-  regl.read(p0)
-  console.log(p0)
+    void main () {
+      vec4 each = texture2D(src, vec2(0.));
+      
+      gl_PointSize = 100.0;
+      gl_Position = vec4(0., each.x, 0., 1.);
+    } 
+  `,
+  frag: `
+    void main () {
+      gl_FragColor = vec4(1., 0., 0., 1.); 
+    } 
+  `,
+  uniforms: {
+    src: regl.prop('src') 
+  },
+  primitive: 'point',
+  count: 1
 })
+
+var k = 3
+var mass = 0.5
+var damping = 0.2
+var h = 0.1
+var t = 0
+var x = 1
+var v = 0.2
+var side = 1
+var p0 = new Float32Array(side * side * 4)
+
+for ( var i = 0; i < p0.length; i+=4 ) {
+  p0[i] = x
+  p0[i + 1] = v
+}
+
+var src = regl.framebuffer({
+  color: regl.texture({
+    data: p0,
+    shape: [ side, side ]
+  }),
+  colorFormat: 'rgba',
+  colorType: 'float',
+  depth: false
+})
+var dest = regl.framebuffer({
+  color: regl.texture({
+    data: p0,
+    shape: [ side, side ]
+  }),
+  colorFormat: 'rgba',
+  colorType: 'float',
+  depth: false
+})
+var tmp = null
+
+requestIdleCallback(main)
+
+function main () {
+  var prev = Date.now()
+  var current = Date.now() 
+  var dT = 0
+
+  regl.frame(function ({ tick }) {
+    prev = current
+    current = Date.now()
+    dT = current - prev
+    t += dT / 1000
+    regl.clear({
+      color: [ 0, 0, 0, 0 ] 
+    })
+    step({ h, t, k, mass, damping, side, src, dest })
+    draw({ src: dest })
+    tmp = src
+    src = dest
+    dest = tmp
+  })
+}
